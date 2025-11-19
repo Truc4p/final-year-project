ee34932c620411d80c4515e4b8be0384908602e5
Truc1p <truc4pham@gmail.com>
2025-11-19 15:04:42 +0700

    commit mobile admin

---
+++ mobile-app-admin/src/constants/agora.js
@@ -0,0 +1,5 @@
+// Agora.io configuration
+export const AGORA_APP_ID = 'ab77ef835e764c7eb51ca96a44cbe785';
+
+// Channel naming convention: use stream ID as channel name
+export const getChannelName = (streamId) => `wrencos-${streamId}`;
